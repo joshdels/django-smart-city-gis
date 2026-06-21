1c896d7c5733de93675cf3cbc18f918b0a53d3c9
joshdels <assistantgisjosh@gmail.com>
2026-06-21 14:55:24 +0800

    parcel models"

--- apps/parcels/models.py
+++ apps/parcels/models.py
@@ -1,3 +1,43 @@
+from django.contrib.gis.db import models as gis_models
 from django.db import models
 
-# Create your models here.
+
+class Owner(models):
+    owner_id = models.AutoField(primary_key=True)
+
+    first_name = models.CharField(max_length=200)
+    middle_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=200)
+
+    status = models.CharField(max_length=100, default="active")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Parcels(models.Model):
+    parcel_id = models.idField(max_length=200, unique=True)
+
+    geom = models.PolygonField(srid=4326)
+
+    ownership = models.ForeignKey(
+        Owner, on_delete=models.SET_NULL, null=True, blank=True, related_name="parcels"
+    )
+
+    status = models.CharField(max_length=100)
+    paid_status = models.BooleanField(default=False)
+
+
+class Boundary(models.Model):
+    region_name = models.CharField(max_length=255)
+    city_name = models.CharField(max_length=255)
+    barangay_name = models.CharField(max_length=255)
+
+    region_code = models.CharField(max_length=50, blank=True, null=True)
+    city_code = models.CharField(max_length=50, blank=True, null=True)
+    barangay_code = models.CharField(max_length=50, blank=True, null=True)
+
+    geom = gis_models.MultiPolygonField(srid=4326)
+
+    def __str__(self):
+        return f"{self.barangay_name}, {self.city_name}, {self.region_name}"
